35aeefc87c91086e378d67a58fe6768acb1b82e1
Carlos Augusto <augustino2003@hotmail.com>
2021-07-07 16:16:52 -0300

    Offer

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Course.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Course.java
@@ -1,34 +1,41 @@
 package com.devsuperior.dslearnbds.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_course")
 public class Course implements Serializable{
-
 	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String imgUri;
 	private String imgGrayUri;
+	
+	@OneToMany(mappedBy = "course")
+	private List<Offer> offers = new ArrayList<>();
+	
 	public Course() {
-		super();
 	}
+	
 	public Course(Long id, String name, String imgUri, String imgGrayUri) {
-		super();
 		this.id = id;
 		this.name = name;
 		this.imgUri = imgUri;
 		this.imgGrayUri = imgGrayUri;
 	}
+	
 	public Long getId() {
 		return id;
 	}
@@ -53,6 +60,11 @@ public class Course implements Serializable{
 	public void setImgGrayUri(String imgGrayUri) {
 		this.imgGrayUri = imgGrayUri;
 	}
+	
+	public List<Offer> getOffers() {
+		return offers;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -15,3 +15,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course(name, img_Uri, img_Gray_Uri) values ('Bootcamp HTML', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1xW9YuiAfCf-E0HVQcJFREGDDImkzirTpjPzfrVyvbdZzP9KR1GmtiowkoJeso7nJcqo&usqp=CAU', 'https://image.shutterstock.com/image-vector/elearning-icon-online-education-symbol-260nw-1923064007.jpg');
 
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z', 1);
